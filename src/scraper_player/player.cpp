f0cb33bad7e5a83bef4a5050c0372be55f1adb8f
Michał Łazowik <mlazowik@gmail.com>
2016-05-27 13:48:54 +0200

    Parse metadata interval

--- src/scraper_player/player.cpp
+++ src/scraper_player/player.cpp
@@ -12,6 +12,33 @@
 
 const std::string EOL = "\r\n";
 
+int toInt(std::string numberString) {
+    int value;
+    std::string::size_type first_after_number;
+
+    try {
+        value = std::stoi(numberString, &first_after_number);
+    } catch (std::invalid_argument &ex) {
+        throw std::invalid_argument(numberString + " is not a number");
+    } catch (std::out_of_range &ex) {
+        throw std::out_of_range(numberString + " is out of range");
+    }
+
+    if (first_after_number != numberString.length()) {
+        throw std::invalid_argument(numberString + " is not a number");
+    }
+
+    return value;
+}
+
+bool startsWith(const std::string &string, const std::string &prefix) {
+    if (string.length() < prefix.length()) {
+        return false;
+    }
+
+    return std::equal(prefix.begin(), prefix.end(), string.begin());
+}
+
 bool endsWith(const std::string &string, const std::string &suffix) {
     if (string.length() < suffix.length()) {
         return false;
@@ -77,17 +104,32 @@ int main(int argc, char* argv[]) {
 
         reader = getLineReader(radio);
 
+        int metadataInterval = 0;
+
         events.registerDescriptor(&radio, [&](Descriptor *socket, short revents) {
             reader->readNextChunk();
 
             if (reader->finished()) {
-                std::cerr << reader->getValue();
+                std::string line = reader->getValue();
+                delete reader;
 
-                if (reader->getValue() == "\r\n") {
+                std::cerr << line;
+
+                std::string metadataIntervalHeader = "icy-metaint:";
+
+                if (startsWith(line, metadataIntervalHeader)) {
+                    std::string numberString = line.substr(
+                            metadataIntervalHeader.length(),
+                            line.length() - metadataIntervalHeader.length() - 2
+                    );
+
+                    metadataInterval = toInt(numberString);
+                }
+
+                if (line == EOL) {
                     events.removeDescriptor(&radio);
-                    delete reader;
+                    std::cerr << "Interval: " << metadataInterval << "\n";
                 } else {
-                    delete reader;
                     reader = getLineReader(radio);
                 }
             }
